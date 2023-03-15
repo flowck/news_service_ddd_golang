3eea9150125ac84f5c51f4974f537519a61c6b2b
Firmino Changani <firmino.changani@gmail.com>
2023-03-15 23:38:23 +0000

    handle term signals

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,14 +2,57 @@ package main
 
 import (
 	"github.com/flowck/news_service_ddd_golang/internal/common/logs"
+	"github.com/kelseyhightower/envconfig"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
 	Version = "" // injected during build
 )
 
-func main() {
-	logger := logs.New(true)
+type Config struct {
+	Port      int16  `envconfig:"PORT"`
+	DebugMode string `envconfig:"FLAG_DEBUG_MODE"`
+}
 
+func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	logger := logs.New(cfg.DebugMode == "enabled")
 	logger.Info("news_service")
+
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done
+	logger.Info("Preparing to shutdown gracefully")
+
+	// ctxTerm, cancelCtxTerm := context.WithTimeout(ctx, time.Second*15)
+	// defer cancelCtxTerm()
+
+	logger.Info("The service has been terminated")
+}
+
+func getConfig() (*Config, error) {
+	cfg := &Config{}
+
+	if err := envconfig.Process("", cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func fatalOnErr(logger *logs.Logger, err error) {
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
