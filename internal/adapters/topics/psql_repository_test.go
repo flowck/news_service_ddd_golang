69b2ee5ff48b26f52a34de40877f0d23580f83fe
Firmino Changani <firmino.changani@gmail.com>
2023-03-19 01:16:01 +0000

    create adapter to insert a topic #22 #20 (#65)

---
+++ internal/adapters/topics/psql_repository_test.go
@@ -0,0 +1,65 @@
+package topics_test
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/brianvoe/gofakeit"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/flowck/news_service_ddd_golang/internal/adapters/topics"
+	"github.com/flowck/news_service_ddd_golang/internal/common/psql"
+	"github.com/flowck/news_service_ddd_golang/internal/domain"
+	"github.com/flowck/news_service_ddd_golang/internal/domain/news"
+)
+
+var (
+	db          *sql.DB
+	ctx         context.Context
+	testTimeout = time.Minute * 2
+)
+
+func TestTopicsRepositoryLifecycle(t *testing.T) {
+	repo, err := topics.NewPsqlRepository(db)
+	require.Nil(t, err)
+
+	// Insert
+	topic := fixtureTopic(t)
+	assert.Nil(t, repo.Insert(ctx, topic))
+}
+
+func TestMain(m *testing.M) {
+	gofakeit.Seed(0)
+
+	tmpctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+	ctx = tmpctx
+
+	var err error
+	db, err = psql.Connect(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = psql.ApplyMigrations(db, "../../../misc/sql/migrations"); err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func fixtureTopic(t *testing.T) *news.Topic {
+	n, err := news.NewTopic(
+		domain.NewID(),
+		gofakeit.BeerAlcohol(),
+	)
+	require.Nil(t, err)
+
+	return n
+}
