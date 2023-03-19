20d10e5a9a9a31385660e8a4179e662881eb2035
Firmino Changani <firmino.changani@gmail.com>
2023-03-19 18:10:30 +0000

    implement component tests for create topic #21

---
+++ tests/components/topics_test.go
@@ -0,0 +1,21 @@
+package components
+
+import (
+	"github.com/brianvoe/gofakeit"
+	"github.com/flowck/news_service_ddd_golang/tests/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+func TestTopics(t *testing.T) {
+	// create topic
+	res01, err := getClient(t).CreateTopic(ctx, fixtureTopic(t))
+	require.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res01.StatusCode)
+}
+
+func fixtureTopic(t *testing.T) client.CreateTopicRequest {
+	return client.CreateTopicRequest{Name: gofakeit.BeerName()}
+}
